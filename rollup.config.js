b3dcd502cc6e9c1cfaf8ae99e1158de77c527020
alioguzhan <aoguzhanyildiz@gmail.com>
2020-09-04 18:10:48 +0300

    fix: upgrade deps to fix vulnerabilities
    also update the rollup config for the new version

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import typescript from 'rollup-plugin-typescript2';
 import commonjs from 'rollup-plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
 import resolve from 'rollup-plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
 
 import pkg from './package.json';
 
@@ -11,14 +12,22 @@ export default {
     {
       file: pkg.main,
       format: 'cjs',
-      exports: 'named',
       sourcemap: true,
+      plugins: [terser()],
+      exports: 'named',
     },
     {
       file: pkg.module,
       format: 'es',
-      exports: 'named',
       sourcemap: true,
+      plugins: [terser()],
+      exports: 'named',
+    },
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: true,
+      exports: 'named',
     },
   ],
   plugins: [
@@ -26,7 +35,7 @@ export default {
     resolve(),
     typescript({
       rollupCommonJSResolveHack: true,
-      exclude: ['**/__tests__/**', '*.spec.*'],
+      exclude: ['**/__tests__/**', '*.spec.*', '*.test.*'],
       clean: true,
     }),
     commonjs({
